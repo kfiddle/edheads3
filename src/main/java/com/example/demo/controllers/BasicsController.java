3eb4d456abf846ce20a062dfb7311b8ef076ac1d
kfiddle <kenjfiddle@gmail.com>
2021-08-18 13:02:51 -0400

    @modelAttribute in basics controller

--- src/main/java/com/example/demo/controllers/BasicsController.java
+++ src/main/java/com/example/demo/controllers/BasicsController.java
@@ -3,11 +3,13 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.User;
 import com.example.demo.repositories.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+
 
 @Controller
 public class BasicsController {
@@ -23,8 +25,27 @@ public class BasicsController {
     @RequestMapping("/register")
     public String showRegistrationForm(Model model) {
         model.addAttribute("user", new User());
-
         return "signup_form";
     }
+
+    @RequestMapping("/register_success")
+    public String showSuccessfulRegistrationForm() {
+        return "register_success";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(@ModelAttribute User user) {
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        
+
+        userRepo.save(user);
+        return "redirect:register_success";
+    }
+
+
+
 
 }
