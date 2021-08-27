d82a5a0557523c67fab45da89513d784f8607eb5
First Last <samoviskik@gmail.com>
2021-08-27 18:04:28 -0400

    got nested accordions to work

--- src/main/resources/static/js/accordion.js
+++ src/main/resources/static/js/accordion.js
@@ -1,6 +1,4 @@
 let acc = document.getElementsByClassName("accordion");
-// let acc = document.getElementsByClassName("accordionInterior");
-
 
 for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
@@ -13,19 +11,19 @@ for (let i = 0; i < acc.length; i++) {
         }
     });
 }
-//
-// let acc = document.getElementsByClassName("accordionInterior");
-//
-// for (let i = 0; i < acc.length; i++) {
-//     acc[i].addEventListener("click", function() {
-//         this.classList.toggle("active");
-//         let panel = this.nextElementSibling;
-//         if (panel.style.display === "block") {
-//             panel.style.display = "none";
-//         } else {
-//             panel.style.display = "block";
-//         }
-//     });
-// }
+
+let nestacc = document.getElementsByClassName("nestedAccordion");
+
+for (let i = 0; i < nestacc.length; i++) {
+    nestacc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
 
 
