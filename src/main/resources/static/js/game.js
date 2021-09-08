e62ef9f230fa1fd71ccfc7867c5507629416173a
First Last <samoviskik@gmail.com>
2021-09-08 11:31:34 -0400

    making working branch with updated front end

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,6 +1,6 @@
 //The openwindow function take a URL as a parameter and opens it in a new pop-up window
 function openwindow(url) {
-    newwindow=window.open(url,'name','height=600,width=600');
+    newwindow=window.open(url,'name','height=100%,width=auto');
     if (window.focus) {newwindow.focus()}
     return false;
 }
