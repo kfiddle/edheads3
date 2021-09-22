89784fade704d7d95f7065500a4468f7e28c649f
kfiddle <kenjfiddle@gmail.com>
2021-09-22 19:13:53 -0400

    ads commented out and script in welcome.html

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,3 +11,9 @@ public class DemoApplication {
     }
 
 }
+
+
+//<script src="//ap.lijit.com/www/delivery/fpi.js?z=134321&width=160&height=600"></script>
+//
+//<script src="//ap.lijit.com/www/delivery/fpi.js?z=134320&width=300&height=250"></script>
+//<script src="//ap.lijit.com/www/delivery/fpi.js?z=203858&width=300&height=250"></script>
